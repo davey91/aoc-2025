2548880bcd2fb654df5738bed0cf4615b239933a
daveybrouwer <>
2025-12-01 19:29:17 +0100

    Solution that works with test-input

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,26 @@
+import scala.annotation.tailrec
+import scala.collection.immutable.{AbstractSeq, LinearSeq}
+import scala.io.Source
+
+object Day1:
+
+  def day1A(): Int =
+    val lines = Source.fromResource("day1/input.txt").getLines().toSeq
+
+    @tailrec
+    def countAccumulatedZeroes(directionAndNumbers: Seq[String], dialRotation: Int = 50, totalZeroes: Int = 0): Int =
+      directionAndNumbers match
+        case Nil => totalZeroes
+        case directionAndNumber :: tail =>
+          val value = directionAndNumber.splitAt(1)._2.toInt
+          val newTotal = if directionAndNumber.startsWith("L") then
+            if (dialRotation - value) < 0 then 100 - Math.abs((dialRotation - value) % 100) else dialRotation - value
+          else if (dialRotation + value) > 99 then 0 + Math.abs((dialRotation + value) % 100) else dialRotation + value
+          val newTotalZeroes = if (newTotal == 0) totalZeroes + 1 else totalZeroes
+          countAccumulatedZeroes(tail, newTotal, newTotalZeroes)
+
+    countAccumulatedZeroes(lines)
+
+  @main
+  def one(): Unit =
+    println(s"1A: ${day1A()}")
