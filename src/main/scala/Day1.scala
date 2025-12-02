2e77cc10578c8ef5e739bc2753be1adddc2379a0
daveybrouwer <>
2025-12-02 20:02:43 +0100

    Fix 1B. Values above 1000 were missing

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,9 +1,8 @@
 import scala.annotation.tailrec
-import scala.collection.immutable.{AbstractSeq, LinearSeq}
 import scala.io.Source
 
 object Day1:
-  val lines = Source.fromResource("day1/test-input.txt").getLines().toSeq
+  val lines = Source.fromResource("day1/input.txt").getLines().toSeq
 
   def day1A(): Int =
 
@@ -28,22 +27,18 @@ object Day1:
       directionAndNumbers match
         case Nil => totalClicks
         case directionAndNumber :: tail =>
-          // add value > 1000 to clicks
           val extraClicks = directionAndNumber.splitAt(1)._2.toInt % 1000 / 100
           val value = directionAndNumber.splitAt(1)._2.toInt % 100
           val (newRotation, clicks) = if directionAndNumber.startsWith("L") then
-            if (dialRotation - value) <= 0 then {
-              println(value)
-              val clicks = if(value > 99) 1 + value / 100 else 1
-              (100 - Math.abs((dialRotation - value) % 100), clicks)
-            } else (dialRotation - value, 0)
+            if (dialRotation - value) <= 0 then
+              (100 - Math.abs((dialRotation - value) % 100), 1)
+            else (dialRotation - value, 0)
           else if (dialRotation + value) > 99 then
-            val clicks = if(value > 99) 1 + value / 100 else 1
-            (0 + Math.abs((dialRotation + value) % 100), clicks)
+            (0 + Math.abs((dialRotation + value) % 100), 1)
           else (dialRotation + value, 0)
 
-          val clicksToAdd = if((dialRotation == 0 || dialRotation == 100) && clicks > 0) clicks - 1 else clicks
-          println(s"startRotation: $dialRotation Direction: $directionAndNumber, Pointed at:$newRotation, new clicks: $clicksToAdd, totalClicks: ${totalClicks + clicksToAdd}")
+          val clicksToAdd = if ((dialRotation == 0 || dialRotation == 100) && clicks > 0) extraClicks + clicks - 1 else extraClicks + clicks
+          //println(s"startRotation: $dialRotation Direction: $directionAndNumber, Pointed at:$newRotation, new clicks: $clicksToAdd, totalClicks: ${totalClicks + clicksToAdd}")
           countAccumulatedZeroes(tail, newRotation, totalClicks + clicksToAdd)
 
     countAccumulatedZeroes(lines)
